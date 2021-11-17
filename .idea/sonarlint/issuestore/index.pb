674757a5e6dc7b7bba9a329eb5b88322d50d5e33
Oscar <os.gomez.lozano@gmail.com>
2021-11-16 19:38:40 -0500

    ending of the interfaz

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,6 +1,4 @@
 
-o
-?app/src/main/java/com/example/pruebamaquetacion/MainActivity.kt,5\4\54e56e691ec28ecc7a6cb80ed400e180386e2fa0
 @
 app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
 :
---
+++ app/src/main/java/com/example/pruebamaquetacion/GardensByTheBayActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pruebamaquetacion
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GardensByTheBayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gardens_by_the_bay)
+    }
+}
