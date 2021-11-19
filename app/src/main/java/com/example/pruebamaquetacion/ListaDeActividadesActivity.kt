f1cae044694558f8c73bc37099b5f504f439cbc6
Oscar <os.gomez.lozano@gmail.com>
2021-11-18 19:58:58 -0500

    Making an adapter for the recycle view so the data can be replaced on each view

---
+++ app/src/main/java/com/example/pruebamaquetacion/ListaDeActividadesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pruebamaquetacion
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ListaDeActividadesActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_de_actividades)
+    }
+}
---
+++ app/src/main/java/com/example/pruebamaquetacion/TouristSiteList.kt
@@ -0,0 +1,9 @@
+package com.example.pruebamaquetacion
+
+data class TouristSiteList(
+    val name : String,
+    val description : String,
+    val location : String,
+    val temperature : String,
+    val recomendations : String
+)
