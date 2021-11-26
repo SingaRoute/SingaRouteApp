eae7c7c337e822ce1ee0c604f604328fc8b3cb1b
Oscar <os.gomez.lozano@gmail.com>
2021-11-26 18:25:32 -0500

    binding the data of the detail

--- singaroute/src/main/java/com/grupoandroid/singaroute/detail/DetailFragment.kt
+++ singaroute/src/main/java/com/grupoandroid/singaroute/detail/DetailFragment.kt
@@ -5,20 +5,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.grupoandroid.singaroute.databinding.FragmentDetailBinding
+import com.squareup.picasso.Picasso
 
 class DetailFragment : Fragment() {
 
     private lateinit var detailBinding: FragmentDetailBinding
+    private val args: DetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         detailBinding = FragmentDetailBinding.inflate(inflater, container, false)
 
         return  detailBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val place = args.place
+
+        with(detailBinding){
+            tittleTextView.text = place?.tittle
+            descriptionTextView.text = place?.description
+            scheduleTextView.text = place?.schedule
+            locationTextView.text = place?.location
+            Picasso.get().load(place?.urlPicture).into(locationImageView)
+        }
     }
 
 }
