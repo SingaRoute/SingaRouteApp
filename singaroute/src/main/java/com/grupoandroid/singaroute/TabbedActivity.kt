06bf032f42d5962298b63f7e579bcf611e0b4bb6
Oscar <os.gomez.lozano@gmail.com>
2021-12-08 15:16:35 -0500

    optimizing imports

--- singaroute/src/main/java/com/grupoandroid/singaroute/TabbedActivity.kt
+++ singaroute/src/main/java/com/grupoandroid/singaroute/TabbedActivity.kt
@@ -1,13 +1,7 @@
 package com.grupoandroid.singaroute
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import com.grupoandroid.singaroute.ui.main.SectionsPagerAdapter
 import com.grupoandroid.singaroute.databinding.ActivityTabbedBinding
 
@@ -24,8 +18,6 @@ class TabbedActivity : AppCompatActivity() {
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         binding.viewPager.adapter = sectionsPagerAdapter
         binding.tabs.setupWithViewPager(binding.viewPager)
-
-
 
     }
 }
